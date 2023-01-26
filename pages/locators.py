ff6f29f6acd4c9629844c6a9982108bf64b179e9
Дарья Пилипчук <darapilipcuk@MacBook-Pro-Dara.local>
2023-01-26 11:43:42 +0300

    new versions

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators():
 class LoginPageLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+
+
+class BasketPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    BASKET_NAME = (By.CSS_SELECTOR, "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > h1")
+    BASKET_ADD = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    BASKET_VALUE = (By.CSS_SELECTOR, "#content_inner > article > div:nth-child(1) > div.col-sm-6.product_main > "
+                                     "p.price_color")
+    BASKET_VALUE_ADD = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > "
+                                         "p:nth-child(1) > strong")
